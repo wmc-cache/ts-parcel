4cce5f2273b106a5fe36e2251f5229974fe4cdde
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-01-05 21:01:26 +0800

    add promise

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -119,9 +119,6 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"src/index.ts":[function(require,module,exports) {
 "use strict";
-
-var a = 4;
-console.log(a);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -150,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59691" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61296" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ src/heightType.ts
@@ -0,0 +1,34 @@
+
+/**
+ *  Record 用法
+ */
+ interface PageInfo {
+    title: string;
+  }
+  
+  type Page = "home" | "about" | "contact";
+  
+  const nav: Record<Page, PageInfo> = {
+    about: { title: "about" },
+    contact: { title: "contact" },
+    home: { title: "home" },
+  };
+
+  /**
+   *  Keyof 用法
+   */
+  interface Test{
+
+    webName:string;
+  
+    age:number;
+  
+    address:string
+  
+  }
+  
+  type ant = keyof Test;
+  
+  let add:ant = 'webName'
+
+  export {}
