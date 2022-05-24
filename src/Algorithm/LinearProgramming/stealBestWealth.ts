1607bc6be50743929c2f8020eecdc990dfbad78a
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-05-24 18:48:08 +0800

    add

--- src/Algorithm/LinearProgramming/stealBestWealth.ts
+++ src/Algorithm/LinearProgramming/stealBestWealth.ts
@@ -1,9 +1,13 @@
-// [2, 3, 4, 5]
-export function stealBestWealth(arr: number[]):number {
-  let n = arr.length;
+//
+export function stealBestWealth(arr: number[]): number {
+  let n = arr.length
   if (n === 1) return arr[0]
-  return  arr[0] + stealBestWealth(arr.slice(1))
+  let temp = 0
+  for (let i = 0; i < n - 2; i++) {
+    //console.log(i, arr[i], arr, arr.slice(i + 2))
+    temp = Math.max(temp, arr[i] + stealBestWealth(arr.slice(i + 2)))
+  }
+  return temp
 }
 
-
-console.log(stealBestWealth([2, 3, 4, 5]));
+console.log(stealBestWealth([2, 3, 4, 1, 5]))
