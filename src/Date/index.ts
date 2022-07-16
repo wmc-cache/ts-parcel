54c7a79d4b73262827c225ca581e5a33a0a3fc6e
吴梦成 <wumengcheng@192.168.124.8>
2022-07-16 12:13:59 +0800

    add

---
+++ src/Date/index.ts
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param start 
+ * @param end 
+ */
+// 二个日期之间的天数 
+export function getTime(start: string, end: string) {
+  let preTime: Date = new Date(start)
+  let nowTime: Date = new Date(end)
+  let preTimeNums = Date.parse(preTime.toString()) //parse() 方法可解析一个日期时间字符串，并返回 1970/1/1 午夜距离该日期时间的毫秒数。
+  let nowTimeNums = Date.parse(nowTime.toString())
+  let diffTimeNums = Math.abs(nowTimeNums - preTimeNums)
+  let day = Math.floor(diffTimeNums / (1000 * 60 * 60 * 24))
+  let hour = Math.floor(diffTimeNums / (1000 * 60 * 60)) - day * 24
+  let minute =
+    Math.floor(diffTimeNums / (1000 * 60)) - day * 24 * 60 - hour * 60
+  let second =
+    Math.floor(diffTimeNums / 1000) -
+    day * 24 * 60 * 60 -
+    hour * 60 * 60 -
+    minute * 60
+  console.log(day, hour, minute, second)
+}
+// getTime('2022-07-08 18:00:00', new Date().toString())
+console.log(new Date().toString())
+getTime(new Date().toString(), '2022-07-08 18:00:00')
