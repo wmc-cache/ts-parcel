5952d9227591cae5550224f4ebdb9534ba5e9e54
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-08-02 16:36:59 +0800

    add

--- src/util/tokenImg.ts
+++ src/util/tokenImg.ts
@@ -1,20 +1,18 @@
-const token = ''
-const url = ''
-const img = document.getElementById('img') as HTMLImageElement
-const request = new XMLHttpRequest()
-request.responseType = 'blob'
-request.open('get', url, true)
-request.setRequestHeader('Authorization', token)
-request.onreadystatechange = (e) => {
-  if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
-    // 生成 blobURL，createObjectURL 方法创建从 URL 到 Blob 对象的映射关系
-    img.src = URL.createObjectURL(request.response)
-    img.onload = () => {
-      URL.revokeObjectURL(img.src)
+export function getTokenImg(token: string, url: string) {
+  const img = document.createElement('img')
+  const request = new XMLHttpRequest()
+  request.responseType = 'blob'
+  request.open('get', url, true)
+  request.setRequestHeader('Authorization', token)
+  request.onreadystatechange = (e) => {
+    if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+      // 生成 blobURL，createObjectURL 方法创建从 URL 到 Blob 对象的映射关系
+      img.src = URL.createObjectURL(request.response)
+      img.onload = () => {
+        URL.revokeObjectURL(img.src)
+      }
     }
   }
+  request.send(null)
+  return img
 }
-request.send(null)
-
-export{}
-
