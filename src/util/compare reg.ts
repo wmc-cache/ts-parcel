55cb47199878a22878b076dc5a1d58e873a9d0ee
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-09-15 11:49:17 +0800

    wmc/消息中心

---
+++ src/util/compare reg.ts
@@ -0,0 +1,17 @@
+export function compare(target: string, str: string, ignore: boolean = false) {
+  let position: Array<number> = [];
+  let index = 0;
+  let reg;
+  if (ignore) {
+    reg = new RegExp(str, "gi");
+  } else {
+    reg = new RegExp(str, "g");
+  }
+  target.replace(reg, function (match: string, pos: number, orginText: string) {
+    position[index] = pos;
+    index++;
+    return "a";
+  });
+  return position;
+}
+console.log(compare("tttAta", "a"));
