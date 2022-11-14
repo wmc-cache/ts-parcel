c19b2a7f0f92f47eb9063cc4e4edc3c2b542a0d8
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-11-14 15:02:24 +0800

    add

--- src/Design/Model/Iterator.ts
+++ src/Design/Model/Iterator.ts
@@ -1,5 +1,5 @@
 class myIterator {
-  constructor(private collection: any[]) {}
+  constructor(private collection: any[]) { }
   private index = 0
   next() {
     if (this.index < this.collection.length) {
@@ -17,3 +17,6 @@ const myiterator = new myIterator([1, [1, 2, 3], 3])
 for (const item of myiterator) {
   console.log(item)
 }
+
+
+
