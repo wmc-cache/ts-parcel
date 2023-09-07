dc383d06960b4f35351e869190d7bd3fb7e4ca71
wumengcheng <1909791074@qq.com>
2023-09-07 17:41:11 +0800

    add

--- src/util/downloadFile.ts
+++ src/util/downloadFile.ts
@@ -22,3 +22,28 @@ export default function downloadFile(url: string, fileName: string) {
     aLink.dispatchEvent(new MouseEvent('click', {}))
   }
 }
+
+
+
+// export default function downloadFile(url, fileName, params = {}) {
+//   const request = new XMLHttpRequest();
+//   request.responseType = 'blob';
+//   request.open('post', url, true);
+//   request.setRequestHeader('Content-Type', 'application/json'); // 设置请求头，根据需要设置合适的 Content-Type
+
+//   request.onreadystatechange = (e) => {
+//     if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+//       // 生成 blobURL
+//       download(URL.createObjectURL(request.response), fileName);
+//     }
+//   };
+
+//   request.send(JSON.stringify(params)); // 将参数转换为 JSON 字符串并发送请求
+
+//   const download = (url, name) => {
+//     const aLink = document.createElement('a');
+//     aLink.download = name;
+//     aLink.href = url;
+//     aLink.dispatchEvent(new MouseEvent('click', {}));
+//   };
+// }
